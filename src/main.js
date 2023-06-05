a30fee48759d80fd90af7cc8895bf56b1cdf018b
tim loozen <timloozen99@gmail.com>
2023-06-05 10:23:17 +0200

    feature: build prod

--- src/main.js
+++ src/main.js
@@ -4,11 +4,11 @@ import {createRouter , createWebHistory} from "vue-router";
 
 import App from './App.vue'
 import './assets/main.css'
-import './node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './node_modules/jquery/dist/jquery.min.js'
-import './node_modules/popper.js/dist/popper.js';
-import './node_modules/bootstrap/dist/js/bootstrap.min.js'
-import './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '/node_modules/jquery/dist/jquery.min.js'
+import '/node_modules/popper.js/dist/popper.js';
+import '/node_modules/bootstrap/dist/js/bootstrap.min.js'
+import '/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
 
 
 
