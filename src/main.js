e6bf395cbc6589d9ac0fe5f495a42232345ac134
tim loozen <timloozen99@gmail.com>
2023-06-05 10:18:40 +0200

    feature: build prod

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import {createRouter , createWebHistory} from "vue-router";
 
+
 import App from './App.vue'
 import './assets/main.css'
 import '/node_modules/bootstrap/dist/css/bootstrap.min.css'
