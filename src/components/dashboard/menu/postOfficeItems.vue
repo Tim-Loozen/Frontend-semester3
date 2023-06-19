5c317cf4c506b4a3be134ae3ea7702cb30cafe70
tim loozen <timloozen99@gmail.com>
2023-06-19 13:55:44 +0200

    remove accounts from post account

--- src/components/dashboard/menu/postOfficeItems.vue
+++ src/components/dashboard/menu/postOfficeItems.vue
@@ -11,10 +11,6 @@
     <li class="nav-item ms-1"><a class="nav-link" href="/dashboard/routes/create">Aanmaken</a></li>
     <li class="nav-item ms-1"><a class="nav-link" href="/dashboard/post-companies/routes/requests">Verzoeken</a></li>
     <li class="nav-item ms-1"><a class="nav-link" href="/dashboard/routes/requests">Historie</a></li>
-    <li class="nav-item mt-1"><h5><b>Accounts</b></h5></li>
-    <hr class="my-1">
-    <li class="nav-item ms-1"><a class="nav-link" href="/dashboard/post-companies/accounts">Overzicht</a></li>
-    <li class="nav-item ms-1"><a class="nav-link" href="/dashboard/post-companies/account/create/:id">Aanmaken</a></li>
   </ul>
 </template>
 
